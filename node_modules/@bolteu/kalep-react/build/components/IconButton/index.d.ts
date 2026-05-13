3f8dd23e29041634ef2afd68bd43aeecffb20ea8
r-paulin <paulins.reinis@gmail.com>
2026-05-13 12:34:08 +0300

    Canonical repo for deploy fix

--- node_modules/@bolteu/kalep-react/build/components/IconButton/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { IconButton } from "./IconButton";
-export type { IconButtonProps } from "./IconButton";
-//# sourceMappingURL=index.d.ts.map
--- node_modules/@bolteu/kalep-react/build/components/baseui-components/Spinner/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Spinner } from "./Spinner";
-//# sourceMappingURL=index.d.ts.map
